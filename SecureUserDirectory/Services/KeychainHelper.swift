01b9640cd9c25a37db8c926f8987a6e4931a23ca
shuvokarmakar011 <shuvo.karmakar011@gmail.com>
2025-09-03 11:49:11 +0600

    - added keychain operations for save, retrieve and delete session token.

---
+++ SecureUserDirectory/Services/KeychainHelper.swift
@@ -0,0 +1,63 @@
+//
+//  KeychainHelper.swift
+//  SecureUserDirectory
+//
+//  Created by Shuvo on 3/9/25.
+//
+
+import Foundation
+import Security
+
+class KeychainHelper {
+    static let shared = KeychainHelper()
+    
+    private let service = "com.irinfosys.SecureUserDirectory"
+    private let account = "sessionToken"
+    
+    // store the token
+    func saveToken(_ token: String) {
+        guard let data = token.data(using: .utf8) else { return }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: data
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status != errSecSuccess {
+            print("Error saving to Keychain: \(status)")
+        }
+    }
+    
+    // retrieves token or return nil if not found
+    func getToken() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnData as String: true
+        ]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        
+        if status == errSecSuccess, let data = item as? Data {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+    
+    // clears token on logout
+    func deleteToken() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
